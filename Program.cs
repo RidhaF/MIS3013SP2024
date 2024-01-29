0427456885f8f33a8af69074361893e467ab6dbb
RidhaF <158059733+RidhaF@users.noreply.github.com>
2024-01-29 08:24:46 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿//IS 3013 005
+//Jan 29 2024
+//Ju Xinglong
+//123111
+
+//0
+//comments
+string str1;
+str1 = "This is a string";
+Console.WriteLine(); // Code statement #//function name ; WriteLine, machine, coffee maker
+//(*):* parsmeters , ingredients
+string str2;
+str2 = Console.ReadLine(); //a function call
+//CPU : calculation 
+//RAM : memory
+
+
+Console.WriteLine("the user output is");
+Console.WriteLine(str2);
+
+string str3;
+str3 = "20";
+
+
+int age1; //int is the room, age1 is the variable 
+double w1; //double room type, w1 is card 
+
+age1 = Convert.ToInt32(str1);
+Console.WriteLine(age1 + 2); 
+
+///int is the room,
+string str4;
+str4 = Console.ReadLine();
+int age2;
+age2 = Convert.ToInt32(str4);
+Console.WriteLine(age2 + 10);
